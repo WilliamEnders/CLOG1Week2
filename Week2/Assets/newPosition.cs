2b637d75627762988eac805626e5fa018e60db2d
William Enders <14hourlunchbreak@gmail.com>
2015-09-10 14:05:27 -0400

    Goldbergggg

---
+++ Week2/Assets/newPosition.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class newPosition : MonoBehaviour {
+	private float Speed = .01f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.position =  gameObject.transform.position + new Vector3 (0.1f, 0.1f, 0);
+	
+		gameObject.transform.Rotate(Vector3.forward);
+	}
+}
